3b6a72d3e1cde5c1a1b60be765cd2304d38ce29f
kaithaanilkumar <kaithaanilyadav@gmail.com>
2023-12-15 18:58:10 +0530

    the java program

---
+++ Automatic_test.java
@@ -0,0 +1,26 @@
+class Automatic_test
+{
+	public static void main(String args[])
+	{
+		int num=6,sum=0,temp=num,rem=0;
+				sum=num*num;
+				num++;
+			
+			System.out.println(sum);
+				for(;sum>0;sum/=10)
+				{
+					rem=sum%100;
+					break;
+				}
+				System.out.println(rem);
+				if(rem==temp)
+				{
+					System.out.println("automatic number");			
+				}
+				else
+				{
+					System.out.println("not automatic number");
+				}
+				
+	}
+}
