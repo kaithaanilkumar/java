6dfa35361e2a7cbf1986164c55c4086f701fd142
kaithaanilkumar <kaithaanilyadav@gmail.com>
2023-12-20 19:12:15 +0530

    the application

---
+++ bank_application.java
@@ -0,0 +1,51 @@
+class bank
+{
+	public static void main(String args[])
+	{
+		int bank=1;
+		String name="bank application";
+		String cust_name="K.anil kumar";
+		int deposit=12000;
+		int withdraw=10000;
+		String pin_code="506145";
+		String phone_number="8978020612";
+		int balance=(deposit-withdraw);
+			switch(bank)
+			{
+				case 1:
+				System.out.println(name);
+				brea
+
+				case 2:
+				System.out.println("existed name:"+cust_name+"\n"+"the updated name:ram kumar");
+				
+				
+				case 3:
+				System.out.println(deposit);
+				
+				case 4:
+				System.out.println(withdraw);
+				
+				
+				case 5:
+				System.out.println(pin_code);
+				
+				
+				case 6:
+				System.out.println(phone_number);
+				
+				
+				case 7:
+				System.out.println(balance);
+				break;
+				
+				default:
+				System.out.println("no details are found ");
+
+			
+				
+			}
+			
+		
+	}
+}
